48a8e06fa99f18831e0f3355e90a8ba3e13856e9
William-A-B <wab513@york.ac.uk>
2024-10-07 11:04:39 +0100

    Work on implementing heap

--- Storage/heap/src/heap.c
+++ Storage/heap/src/heap.c
@@ -1,7 +1,52 @@
 #include "heap.h"
 
 static void heap_up(heap_t *heap) {
-	// Write me!
+	// Start with the last element in the heap
+	// If it's the root element, stop
+	// Compare it with its parent
+	// If the parent is smaller or equal, stop
+	// Swap the element with its parent
+	// With the element in its new locaiton, go back to step 2
+	
+	// 0 = even
+	// 1 = odd
+	int32_t n = -1;
+	int32_t element = 0;
+	int32_t parent_element = 0;
+	
+	int32_t * const offsetStore = heap->store - 1;
+	
+	while (heap->store[0] != heap->store[heap->size]) {
+		n = heap->size % 2;
+		
+		// Element is the root element
+		if (heap->store[0] == heap->store[heap->size]) {
+			return;
+		}
+		
+		if (n == 0) {
+			element = heap->store[heap->size];
+			parent_element = heap->store[(heap->size/2)];
+			if (heap->store[heap->size] > heap->store[(heap->size/2)]) {
+				return;
+			}
+			else if (heap->store[heap->size] < heap->store[(heap->size/2)]) {
+				heap->store[(heap->size/2)] = element;
+				heap->store[heap->size] = parent_element;
+			}
+		}
+		else if (n == 1) {
+			element = heap->store[heap->size];
+			parent_element = heap->store[((heap->size-1)/2)];
+			if (heap->store[heap->size] > heap->store[((heap->size-1)/2)]) {
+				return;
+			}
+			else if (heap->store[heap->size] < heap->store[((heap->size-1)/2)]) {
+				heap->store[((heap->size-1)/2)] = element;
+				heap->store[heap->size] = parent_element;
+			}
+		}
+	}
 }
 
 static void heap_down(heap_t *heap) {
