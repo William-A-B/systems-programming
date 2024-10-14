2d496915676560edd138e11db9c5855dbde3ff16
William-A-B <wab513@york.ac.uk>
2024-10-14 13:16:51 +0100

    Complete mempool implementation

--- Allocation/mempool/src/mempool.c
+++ Allocation/mempool/src/mempool.c
@@ -3,8 +3,20 @@
 void *pool_allocate(mempool_t *pool) {
 	// Return the head of the list of blocks
 	// Update the head pointer
+	
+	// Pool empty so return null
+	if (pool->head == 0) {
+		return 0;
+	}
+	
+	mempool_item_t *block = pool->head;
+	pool->head = pool->head->next;
+	return block;
 }
 
 void pool_deallocate(mempool_t *pool, void *block) {
 	// Add the new item to the head of the list
+	mempool_item_t *mempool_block = block;
+	mempool_block->next = pool->head;
+	pool->head = mempool_block;
 }
