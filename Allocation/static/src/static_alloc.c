df16e125dece9e321683cafa66c56e1815675216
William-A-B <wab513@york.ac.uk>
2024-10-14 10:59:41 +0100

    completed stack allocation of lab 5

--- Allocation/static/src/static_alloc.c
+++ Allocation/static/src/static_alloc.c
@@ -3,10 +3,17 @@
 
 // 16KB pool size
 #define STATIC_ALLOC_POOLSIZE 16384UL
+#define STATIC_ALLOC_ALIGNMENT ((size_t) 8)
 
-static uint8_t static_pool[STATIC_ALLOC_POOLSIZE];
+static uint8_t static_pool[STATIC_ALLOC_POOLSIZE] __attribute__ (( aligned(STATIC_ALLOC_ALIGNMENT) ));
 static size_t pool_index = STATIC_ALLOC_POOLSIZE;
 
 void * static_alloc(size_t bytes) {
+	
+	
+	if (bytes < pool_index) {
+		pool_index = (~(STATIC_ALLOC_ALIGNMENT - 1))&(pool_index - bytes);
+		return &pool_index;
+	}
 	return 0;
 }
