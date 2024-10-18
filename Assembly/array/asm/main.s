fd5848eaeeb740ef125aa648b52c5fe371acf711
William-A-B <104766801+William-A-B@users.noreply.github.com>
2024-10-18 20:33:26 +0100

    Complete fibonacci and implement array sum

--- Assembly/array/asm/main.s
+++ Assembly/array/asm/main.s
@@ -8,7 +8,14 @@ array_end
 	EXPORT asm_main
 asm_main
 	; Your code goes here
-
+	LDR r0 =array
+	LDR r1 =array_end
+	
+loop
+	LDR r2, [r0], #1	; Load the value at r0 into r2 and increment r0 by 1
+	ADD r3, r3, r2		; Add r2 and r3 and place it in r3
+	CMP r0, r1			; Compare r0 and r1 and see if r0 is at array_end
+	BLO loop			; If r0 < r1 loop
  
 	B .			; Loop forever
 
--- Assembly/fibonacci/asm/main.s
+++ Assembly/fibonacci/asm/main.s
@@ -13,9 +13,11 @@ asm_main
 	MOV r3, #1			; Current value
 loop
 	STR r3, [r0], #4	; Store current value in array then increment by 4 bytes
-	ADD r3, r3, r2		; Compute next value - current value = current value + previous value
-						; Work out a way to update r2 with the previous value without requiring to use r4
-	
+	ADD r4, r3, r2		; Compute next value - current value = current value + previous value
+	MOV r2, r3			; Update r2 with r3 (The previous value)
+	MOV r3, r4			; Update r3 with the new current value from r4
+	CMP r0, r1			; Compare r0 to r1, if r0 < r1 set the N flag
+	BLO loop			; If r0 < r1 go to loop label
 	
 
 	B .			; Loop forever
